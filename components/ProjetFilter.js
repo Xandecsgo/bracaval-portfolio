bff39adc70e6e189dba63c0f8c2552920bd5ad89
Xandecsgo <trafulha1999@hotmail.com>
2023-09-25 21:43:09 +0200

    filter ok

---
+++ components/ProjetFilter.js
@@ -0,0 +1,36 @@
+// FiltreProjetBar.jsx
+
+import React from 'react';
+import styles from '../styles/FiltreProjetBar.module.scss'; // Assurez-vous d'importer les styles
+
+const FiltreProjetBar = ({ categories, selectedCategory, onSelectCategory, isAnimated, isPortfolioSectionVisible }) => {
+    const filterBarClasses = [styles['filter-bar']];
+    
+    if (isAnimated && isPortfolioSectionVisible) {
+        filterBarClasses.push(styles.visible);
+      }
+      
+      
+
+  return (
+    <div className={styles['filter-bar']}>
+      <button
+        className={`${styles.button} ${selectedCategory === null ? styles.active : ''}`}
+        onClick={() => onSelectCategory(null)}
+      >
+        All
+      </button>
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={`${styles.button} ${selectedCategory === category ? styles.active : ''}`}
+          onClick={() => onSelectCategory(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FiltreProjetBar;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,59 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.scss'
-import { useEffect, useState } from 'react';
+// Home.js
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import ProjetCard from '../components/ProjetCard';
 import data from '../databaseProjet.json';
-
+import ProjetFilter from '../components/ProjetFilter';
+import styles from '@/styles/Home.module.scss';
 
 export default function Home() {
-
   const [projets, setProjets] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isPortfolioSectionVisible, setIsPortfolioSectionVisible] = useState(false);
 
   useEffect(() => {
-    // Chargez les projets depuis le fichier JSON ici
     setProjets(data.projets);
+  }, []);
+
+  const categoriesArray = getUniqueCategories(projets);
+
+  function getUniqueCategories(projets) {
+    const categoriesSet = new Set();
+    projets.forEach((projet) => {
+      if (projet.categories) {
+        projet.categories.forEach((category) => {
+          categoriesSet.add(category);
+        });
+      }
+    });
+    return Array.from(categoriesSet);
+  }
+
+  const filterProjetsByCategory = () => {
+    if (!selectedCategory) {
+      return projets;
+    }
+
+    return projets.filter((projet) =>
+      projet.categories && projet.categories.includes(selectedCategory)
+    );
+  };
+
+  useEffect(() => {
+    const checkPortfolioSectionVisibility = () => {
+      const portfolioSection = document.getElementById("portfolio");
+      if (portfolioSection) {
+        const rect = portfolioSection.getBoundingClientRect();
+        setIsPortfolioSectionVisible(rect.top < window.innerHeight);
+      }
+    };
+
+    window.addEventListener("scroll", checkPortfolioSectionVisibility);
+    checkPortfolioSectionVisibility();
+
+    return () => {
+      window.removeEventListener("scroll", checkPortfolioSectionVisibility);
+    };
   }, []);
 
   return (
@@ -20,11 +62,10 @@ export default function Home() {
         <title>Portfolio de Guillaume Bracaval - Direction artistique digitale, graphisme d'interface et UX design</title>
         <meta name="description" content="Designer avec expertise en direction artistique digitale, graphisme d'interface et UX design. Compétences en gestion de projet et communication. Maîtrise des logiciels Adobe et Figma." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="index, follow"></meta>
+        <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.mainContainer}>
-
         <section className="mb76" id={styles.about}>
           <div className={`${styles.top} mb76`}>
             <h1>Directeur artistique,<br />concepteur graphique</h1>
@@ -41,7 +82,8 @@ export default function Home() {
               <path className="cls-2" d="m41.11,57.43L34.23,0l-16.61,2,6.88,57.43,16.61-2Z" />
               <path className="cls-2" d="m61.8,38.96l-13.16-10.37-27.2,34.74,13.16,10.37,27.2-34.74Z" />
               <path className="cls-2" d="m44.96,60.5L10.34,33.21,0,46.41l34.63,27.29,10.33-13.2Z" />
-            </svg></div>
+            </svg>
+          </div>
 
           <div className={styles.aboutImg}>
             <img src="https://zupimages.net/up/23/39/f212.png" alt="Guillaume Bracaval" />
@@ -57,24 +99,34 @@ export default function Home() {
               <a href="">Faites-le moi savoir !</a>
             </div>
           </div>
-
         </section>
 
-        <section>
+        <section className={styles.portfolio}>
           <h2 className="mb76">Portfolio</h2>
 
+          {isPortfolioSectionVisible && (
+            <ProjetFilter
+            categories={categoriesArray}
+            selectedCategory={selectedCategory}
+            onSelectCategory={setSelectedCategory}
+            isAnimated={true}
+            isPortfolioSectionVisible={isPortfolioSectionVisible}
+          />
+          
+          )}
+
           <div id="portfolio">
-            <div className="projet-list">
-              {projets.map((projet) => (
+            <div className={styles.projetlist}>
+              {filterProjetsByCategory().map((projet) => (
                 <ProjetCard key={projet.slug} projet={projet} />
               ))}
             </div>
           </div>
         </section>
-
       </main>
     </>
-  )
+  );
 }
+
 
 
